cdbe89409eb8e7c7ffd08c576c32971fd3de3818
zay <859131154@qq.com>
2019-04-12 21:13:19 +0800

    2222277

--- src/main/java/com/zay/git/GITTestMain.java
+++ src/main/java/com/zay/git/GITTestMain.java
@@ -4,7 +4,7 @@ public class GITTestMain {
 	public static void main(String[] args) {
 		System.out.println("GITTest000");
 		System.out.println("这个是backup文件更改的啦！！！");
-		System.out.println("这个是backup文件更改的啦！！！");//我觉得不会报错
+		System.out.println("这个是backup文件更改的啦！！！");//我觉得不会报错0011222
 		System.out.println("这个是backup文件更改的啦！！！");
 		System.out.println("001号backupupdate啦！！！");
 	}
