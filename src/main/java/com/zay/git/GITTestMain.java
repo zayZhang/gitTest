f51a1d5e43b1cc2de968168f922a1554e523f01f
zay <859131154@qq.com>
2019-04-12 21:05:16 +0800

    这个是backUp工程更改的

--- src/main/java/com/zay/git/GITTestMain.java
+++ src/main/java/com/zay/git/GITTestMain.java
@@ -3,6 +3,9 @@ package com.zay.git;
 public class GITTestMain {
 	public static void main(String[] args) {
 		System.out.println("GITTest000");
+		System.out.println("这个是backup文件更改的啦！！！");
+		System.out.println("这个是backup文件更改的啦！！！");
+		System.out.println("这个是backup文件更改的啦！！！");
 		System.out.println("001号backupupdate啦！！！");
 	}
 }
